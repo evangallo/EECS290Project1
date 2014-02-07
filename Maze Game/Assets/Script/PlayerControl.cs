4a9a841e6860deb413e057de94334b0d41d2d978
Bryan Ogren <bao17@case.edu>
2014-02-06 23:37:37 -0500

    movement will adjust for rotation once rotation is implemented
    I can't find a way to read mouse cursor movement for rotating the
    player.

--- Maze Game/Assets/Script/PlayerControl.cs
+++ Maze Game/Assets/Script/PlayerControl.cs
@@ -3,6 +3,8 @@ using System.Collections;
 
 public class PlayerControl : MonoBehaviour {
 
+	public float speed;
+
 	private float lateralMovement, forwardMovement;
 
 	void Start () {
@@ -12,13 +14,18 @@ public class PlayerControl : MonoBehaviour {
 	void Update () {
 		lateralMovement = Input.GetAxis ("Horizontal");
 		forwardMovement = Input.GetAxis ("Vertical");
+
 		
 	}
 
 	void FixedUpdate () {
-		transform.Translate (new Vector3 (lateralMovement, 0f, forwardMovement));
+		transform.Translate (new Vector3 (Mathf.Cos(transform.localRotation.y) * speed * lateralMovement -
+		                                  Mathf.Sin(transform.localRotation.y) * speed * forwardMovement, 
+		                                  0f, 
+		                                  Mathf.Sin(transform.localRotation.y) * speed * lateralMovement +
+		                     			  Mathf.Cos(transform.localRotation.y) * speed * forwardMovement));
 
 	}
-	
+
 
 }
