b1f397ad6f84cfaaeb245800ed37993727525d9f
Bryan Ogren <bao17@case.edu>
2014-02-07 17:04:14 -0500

    The mouse now tilts and turns the camera
    the motion is relative to the orientation of the player though, so it
    functions kind of like a flight simulator.

--- Maze Game/Assets/Script/PlayerControl.cs
+++ Maze Game/Assets/Script/PlayerControl.cs
@@ -3,9 +3,10 @@ using System.Collections;
 
 public class PlayerControl : MonoBehaviour {
 
-	public float speed;
+	public float speed, rotationSpeed;
 
 	private float lateralMovement, forwardMovement;
+	private float aimHorizontal, aimVertical;
 
 	void Start () {
 
@@ -14,16 +15,35 @@ public class PlayerControl : MonoBehaviour {
 	void Update () {
 		lateralMovement = Input.GetAxis ("Horizontal");
 		forwardMovement = Input.GetAxis ("Vertical");
+		aimHorizontal = Input.mousePosition.x;
+		aimVertical = Input.mousePosition.y;
+
+		if (aimHorizontal < Screen.width / 3 ||
+		    aimHorizontal > Screen.width / 3) {
+			transform.Rotate(new Vector3 (0f, 1f, 0f), 
+			                 (aimHorizontal - Screen.width/2)/Screen.width 
+			                 * rotationSpeed * Time.deltaTime);
+		}
+
+		if (aimVertical < Screen.height / 3 ||
+		    aimVertical > Screen.height / 3 / 3) {
+			transform.Rotate(new Vector3 (-1f, 0f, 0f), 
+			                 (aimVertical - Screen.height/2)/Screen.height 
+			                 * rotationSpeed * Time.deltaTime);
+		}
 
 		
 	}
 
 	void FixedUpdate () {
-		transform.Translate (new Vector3 (Mathf.Cos(transform.localRotation.y) * speed * lateralMovement -
-		                                  Mathf.Sin(transform.localRotation.y) * speed * forwardMovement, 
+		transform.Translate (new Vector3 ((Mathf.Cos(transform.localRotation.y) * lateralMovement -
+		                                  Mathf.Sin(transform.localRotation.y) * forwardMovement) * 
+		                                  speed * Time.deltaTime, 
 		                                  0f, 
-		                                  Mathf.Sin(transform.localRotation.y) * speed * lateralMovement +
-		                     			  Mathf.Cos(transform.localRotation.y) * speed * forwardMovement));
+		                                  (Mathf.Sin(transform.localRotation.y) * lateralMovement +
+		                                  Mathf.Cos(transform.localRotation.y) * forwardMovement) * 
+		                                  speed * Time.deltaTime));
+
 
 	}
 
