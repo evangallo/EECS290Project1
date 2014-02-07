fc2ddf15457ce9db146af9c3f3bd987270f4f1b4
Bryan Ogren <bao17@case.edu>
2014-02-06 22:28:53 -0500

    The player has a spotlight and can move

---
+++ Maze Game/Assets/Script/PlayerControl.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerControl : MonoBehaviour {
+
+	private float lateralMovement, forwardMovement;
+
+	void Start () {
+
+	}
+
+	void Update () {
+		lateralMovement = Input.GetAxis ("Horizontal");
+		forwardMovement = Input.GetAxis ("Vertical");
+		
+	}
+
+	void FixedUpdate () {
+		transform.Translate (new Vector3 (lateralMovement, 0f, forwardMovement));
+
+	}
+	
+
+}
