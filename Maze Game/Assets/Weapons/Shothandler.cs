c94c17f15631114c971a8c8ace4c203640363b8b
iansanderson <isa12@case.edu>
2014-02-12 21:20:41 -0500

    Pushed my local version because something big broke.

---
+++ Maze Game/Assets/Weapons/Shothandler.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Shothandler : MonoBehaviour {
+
+    public Vector3 velocity;
+    public float damage;
+    public Transform prefab;
+	// Use this for initialization
+    void Start()
+    {
+        prefab.rigidbody.AddForce(velocity, ForceMode.VelocityChange);
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+    //Removes the bullet, ends the script, and returns the damage
+    //Current configuration assumes that monsters and walls will handle detecting an impact
+    float OnHit()
+    {
+        enabled = false;
+        return damage;
+    }
+}
