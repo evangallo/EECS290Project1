57e93478b95d4af8d10b48b5348e635189f7d83f
JamesMarsh <james.marsh124@gmail.com>
2014-02-16 22:32:31 -0500

    Bullets now despawn on hitting a wall or time running out
    Signed-off-by: JamesMarsh <james.marsh124@gmail.com>

--- Maze Game/Assets/Scripts/ShotController.cs
+++ Maze Game/Assets/Scripts/ShotController.cs
@@ -3,14 +3,16 @@ using System.Collections;
 
 public class ShotController : MonoBehaviour {
 
+    public float count;
     public Vector3 velocity, position;
     public float damage;
     public Transform prefab;
 	// Use this for initialization
     void Start()
     {
-        prefab.rigidbody.MovePosition(position);
-        prefab.rigidbody.AddForce(velocity, ForceMode.VelocityChange);
+        count = 3.0f;
+        //prefab.rigidbody.MovePosition(position);
+        //prefab.rigidbody.AddForce(velocity, ForceMode.VelocityChange);
 	}
     public ShotController(Vector3 vel, Vector3 pos, float dam, Transform pref)
     {
@@ -22,13 +24,16 @@ public class ShotController : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-
+        count -= Time.deltaTime;
+        if (count <= 0f)
+            Destroy(gameObject);
 	}
-    //Removes the bullet, ends the script, and returns the damage
-    //Current configuration assumes that monsters and walls will handle detecting an impact
-    float OnHit()
+    void OnCollisionEnter(Collision theCollision)
     {
-        enabled = false;
-        return damage;
+        if (theCollision.gameObject.name == "Groucho")
+        {
+            return;
+        }
+        Destroy(gameObject);
     }
 }
