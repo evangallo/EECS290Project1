7c2b47e09a0efedbb4355c8419ffd314831f1f2f
Bryan Ogren <bao17@case.edu>
2014-02-18 14:32:22 -0500

    Added expiration timer and GUItext for batteries

---
+++ Maze Game/Assets/Scripts/FlashlightControler.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FlashlightControler : MonoBehaviour {
+
+	public float batteryLifeIncrement;
+	public GameObject flashLight;
+	public GUIText batteryLifeDisplay;
+
+	private float batteryLife;
+	
+	void Start () {
+		ExtendBatteryLife();
+	}
+	
+	void Update () {
+		if (batteryLife >= 0) {
+			batteryLife -= Time.deltaTime;
+		} else {
+			flashLight.SetActive (false);
+		}
+		batteryLifeDisplay.text = "Battery Life: " + Mathf.Round (batteryLife);
+	}
+
+	void ExtendBatteryLife () {
+		batteryLife += batteryLifeIncrement;
+		flashLight.SetActive (true);
+	}
+}
