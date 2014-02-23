3c5d29b06e8a4a6f1c40b3e52158feb1df8bf290
James Marsh <james.marsh124@gmail.com>
2014-02-23 14:55:45 -0500

    Disabling height testing on the A* pather got it to start calculating paths
    Signed-off-by: James Marsh <james.marsh124@gmail.com>

--- Maze Game/Assets/Scripts/GrouchoPather.cs
+++ Maze Game/Assets/Scripts/GrouchoPather.cs
@@ -11,30 +11,44 @@ public class GrouchoPather : MonoBehaviour {
 	private int currentWaypoint;
 	private CharacterController charController;
 	private float maxWaitTime = 5f;
+    private bool havePath = false; //seemed to be trying to path before grid was calculated
 
 	// Use this for initialization
 	void Start () {
-		// Randomly determine initial target
-		GameObject[] cells = GameObject.FindGameObjectsWithTag("Cell");
-		if(cells.Length == 0){
-			target = transform;
-			Debug.Log ("Had to make a Groucho target himself. QQ");
-		}else{
-			target = cells[Random.Range (0, cells.Length - 1)].transform;
-		}
-		Seeker seeker = GetComponent<Seeker>();
-		seeker.StartPath (transform.position, target.position, OnPathComplete);
-		currentWaypoint = 0;
-		if(speed == null || speed == 0){
-			speed = 1;
-		}
-		charController = GetComponent<CharacterController>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+        BeginPath();
 	}
+    void BeginPath()
+    {
+        if (!havePath)
+        {
+
+            // Randomly determine initial target
+           /* GameObject[] cells = GameObject.FindGameObjectsWithTag("Cell");
+            if (cells.Length == 0)
+            {
+                target = transform;
+               Debug.Log("Had to make a Groucho target himself. QQ");
+            }
+            else
+            {
+                target = cells[Random.Range(0, cells.Length - 1)].transform;
+            }*/
+            target = GameObject.FindGameObjectsWithTag("Player")[0].transform;
+            Seeker seeker = GetComponent<Seeker>();
+            seeker.StartPath(transform.position, target.position, OnPathComplete);
+            currentWaypoint = 0;
+            if (speed == null || speed == 0)
+            {
+                speed = 1;
+            }
+            charController = GetComponent<CharacterController>();
+            havePath = true;
+        }
+    }
 	
 	public void OnPathComplete(Path p){
 		if(p.error)
