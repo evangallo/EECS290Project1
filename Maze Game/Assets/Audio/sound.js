3cbef3fc3b6b0d4edda4b58d57fac75d8ae90d63
iansanderson <isa12@case.edu>
2014-02-25 13:53:06 -0500

    Thomas' Audio for shooting

---
+++ Maze Game/Assets/Audio/sound.js
@@ -0,0 +1,24 @@
+﻿@script RequireComponent(AudioSource)
+ var fireRate : float = 0.14;
+private var nextFire : float = 0.0;
+var front : AudioClip;
+
+var back: AudioClip;
+
+function Update(){
+ 
+Fire();
+ 
+}
+
+function Fire(){
+
+if (Input.GetButton("Fire1") && Time.time>nextFire){
+	nextFire = Time.time + fireRate;
+	audio.Play();
+//	ForceFire();
+
+}
+
+
+}
